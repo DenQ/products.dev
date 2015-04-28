f4d027e49e5d1c63fcba6eb4526582c207aa84aa
DenQ <denquick@gmail.com>
2015-04-28 16:43:07 +0400

    Generated StoreBundle. Created entity Product

--- src/Acme/ProductBundle/Controller/DefaultController.php
+++ src/Acme/ProductBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Acme\ProductBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Acme\StoreBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
@@ -26,6 +27,7 @@ class DefaultController extends Controller
 
     public function createAction()
     {
+
         die('product_create');
     }
 
